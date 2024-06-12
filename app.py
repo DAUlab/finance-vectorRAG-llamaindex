20929addc7d7394203927a29011ead1ecfb7d098
Clément COUCHEVELLOU <clement.couchevellou@telecomnancy.eu>
2024-06-12 16:23:19 -0400

    [+] Memory summarization system + model switching system

--- app.py
+++ app.py
@@ -20,14 +20,21 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     # Home page
-    messages = rag.chat_history
-    return render_template("index.html", messages=messages)
+    messages = rag.messages
+    return render_template("index.html", messages=messages, model=rag.vector_database.settings.llm.model)
 
 @app.route("/add_input", methods=["POST", "GET"])
 def add_input():
     # Process user input
     user_query = request.form["question"]
     rag.respond(user_query)
+    return redirect(url_for("index"))
+
+@app.route("/switch_model", methods=["POST", "GET"])
+def switch_model():
+    # Switch the model
+    model = request.form.get("model")
+    rag.switch_model(model)
     return redirect(url_for("index"))
 
 @app.route("/reset", methods=["POST", "GET"])
@@ -37,4 +44,4 @@ def reset():
 
 # Run app in development mode
 if __name__ == "__main__": 
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
--- rag_public_finance.py
+++ rag_public_finance.py
@@ -34,23 +34,15 @@ embed_model = HuggingFaceEmbedding(
 
 """RAG Settings"""
 
+from groq import Groq as Groq_API
 from llama_index.core import Settings
 from llama_index.core.node_parser import SentenceSplitter, SemanticSplitterNodeParser
 
-# Define the LLM, embedding model and other parameters
-Settings.llm = llm
-Settings.embed_model = embed_model
-Settings.node_parser = SemanticSplitterNodeParser(
-        buffer_size=5,
-        include_prev_next_rel=True,
-        embed_model=embed_model,
-    )
-Settings.num_output = 256
-Settings.context_window = 4096 # Maximum size of the input query
+client = Groq_API(api_key=os.environ.get('GROQ_KEY'))
 
 # Prompt engineering
-def prompt(query):
-    return "<s> [INST] <<SYS>>You are a finance assistant and advisor, your role is to provide simple, clear and concise response to the user question. If the question seems complex, try to resolve it step by step. If you don't know how to respond, just say it, don't try to create or imagine false information. You can use markdown syntax to enhance the look and readability of your answers.<</SYS>>\n\n" + query + "[/INST] "
+def prompt(query, history):
+    return "You are a finance assistant and advisor, your role is to provide clear and concise but detailled response to the user question.\n If the question seems complex, try to resolve it step by step. If you don't know how to respond, just say it, don't try to create or imagine false information.\n Use markdown syntax to enhance the look and readability of your answers.\n\n" + "Conversation history:\n" + history + "\n\nUser question:\n" + query
 
 from llama_index.core import SimpleDirectoryReader, VectorStoreIndex, StorageContext, load_index_from_storage, get_response_synthesizer
 from llama_index.core.retrievers import VectorIndexRetriever
@@ -63,6 +55,17 @@ from llama_index.core.query_engine import RetrieverQueryEngine
 class VectorDatabase:
     # Intialize the vector database from the existing index
     def __init__(self):
+        # Llama-index settings
+        Settings.llm = llm
+        Settings.embed_model = embed_model
+        Settings.node_parser = SemanticSplitterNodeParser(
+                buffer_size=5,
+                include_prev_next_rel=True,
+                embed_model=embed_model,
+            )
+        Settings.num_output = 256
+        Settings.context_window = 4096 # Maximum size of the input query
+        self.settings = Settings
         if not os.path.exists(persist_dir):
             # Load the documents and create the index
             documents = SimpleDirectoryReader(path).load_data()
@@ -89,36 +92,63 @@ class VectorDatabase:
 # RAG chatbot class to respond to the user queries
 class RAG:
     # Initialize the RAG chatbot
-    def __init__(self, vector_database):
+    def __init__(self, vector_database, chat_history="", messages=[]):
+        self.vector_database = vector_database
         # Configure retriever
-        self.retriever = VectorIndexRetriever(
+        retriever = VectorIndexRetriever(
             index=vector_database.index,
             similarity_top_k=15,
             verbose=False,
         )
         # Configure response synthesizer
-        self.response_synthesizer = get_response_synthesizer(
+        response_synthesizer = get_response_synthesizer(
             response_mode="compact",
         )
         # Assemble query engine
         self.query_engine = RetrieverQueryEngine(
-            retriever=self.retriever,
-            response_synthesizer=self.response_synthesizer,
+            retriever=retriever,
+            response_synthesizer=response_synthesizer,
         )
         # Memory of the chatbot
-        self.chat_history = []
+        self.chat_history = chat_history
+        self.messages = messages
         print("RAG chatbot loaded successfully")
+    
+    
+    # Switching model
+    def switch_model(self, model):
+        self.vector_database.__init__(llm=Groq(model=model, api_key=os.environ.get('GROQ_KEY')))
+        self.__init__(self.vector_database, self.chat_history, self.messages)
+        print("Switched to", self.vector_database.settings.llm.model)
+
+    # Chat history summarization
+    def history_input(self, query):
+        history_summarizer = client.chat.completions.create(
+            messages=[{
+                "role": "system",
+                "content": "Just summarize the given conversation, it will be used as context for the ongoing conversation. Focus on the user informations and the last assistant message. You are the assistant.\n\n" + str(self.chat_history),
+            }], model="llama3-8b-8192", temperature=0.2)
+        contextualized_query = client.chat.completions.create(
+            messages=[{
+                "role": "system",
+                "content": "History: \n\n" + str(self.chat_history) + "\n\n\nUser message:\n\n" + query + "\n\n SYSTEM INSTRUCTIONS: If the user question is not higly related to the conversation history JUST REPEAT THE USER MESSAGE AS IS. If the query require context from the history slightly rephrase it into a concise standalone question. Do NOT answer the question.\n\n",
+            }], model="llama3-8b-8192", temperature=0.2)
+        self.chat_history = history_summarizer.choices[-1].message.content
+        enhanced_query = contextualized_query.choices[-1].message.content
+        print('chat history: ' + self.chat_history + '\n------------')
+        print('query: ' + enhanced_query)
+        return enhanced_query
     
     # Respond to the user query
     def respond(self, message):
-        response = markdown.markdown(str(self.query_engine.query(prompt(message))))
-        self.chat_history.append({"user": "User", "text": message})
-        self.chat_history.append({"user": "Assistant", "text": response})
-
-    # Get the chat history
-    def get_chat_history(self):
-        return self.chat_history
+        self.chat_history += "\n\nUser:" + message
+        query = self.history_input(message)
+        response = markdown.markdown(str(self.query_engine.query(prompt(query, self.chat_history))))
+        self.chat_history += "\n\nAssistant:" + response
+        self.messages.append({"user": "User", "text": message})
+        self.messages.append({"user": "Assistant", "text": response})
 
     # Reset the chat history
     def reset_history(self):
-        self.chat_history = []
+        self.chat_history = ""
+        self.messages = []
